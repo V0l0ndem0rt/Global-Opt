9157f5fdd07bfadf9cee10860a6c9875ab70a1a7
Vladimir Kiselev <98582893+V0l0ndem0rt@users.noreply.github.com>
2023-02-06 20:36:41 +0300

    add files

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// 'use strict';
+
+// const btnRight = document.querySelector('#btnRight'),
+//       btnLeft = document.querySelector('#btnLeft'),
+//       disableLeft = document.querySelector('.disable-left'),
+//       disableRight = document.querySelector('.disable-right'),
+//       active = document.querySelector('.active');
+
+
+// btnRight.addEventListener('click', () => {
+
+//     active.classList.toggle ='review__item', 'disable', 'disable-right';
+//     disableRight.className ='review__item';
+
+// });
+
+// btnLeft.addEventListener('click', () => {
+
+//     active.classList.add('disable-left','disable');
+//     active.classList.remove('active');
+
+
+//     disableLeft.classList.add('active');
+//     disableLeft.classList.remove('disable-left','disable');
+// });
